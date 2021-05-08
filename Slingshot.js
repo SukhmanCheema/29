5a1b8f767904c60656d10de91de0976be58ad0cb
SukhmanCheema <monacheema88@gmail.com>
2021-05-08 12:45:25 +0530

    we have placed images of slings

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,9 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.slingImage1=loadImage("sprites/sling1.png");
+        this.slingImage2=loadImage("sprites/sling2.png");
+        this.slingImage3=loadImage("sprites/sling3.png");
         World.add(world, this.sling);
     }
 
@@ -16,11 +19,30 @@ class SlingShot{
     }
 
     display(){
+        image(this.slingImage1,170,20);
+        image(this.slingImage2,140,20);
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x<180){
+             stroke(48,22,8);
+            strokeWeight(7);
+            line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+            image(this.slingImage3,pointA.x-25,pointA.y-15,15,30);
+            } else{
+
+                stroke(48,22,8);
+                strokeWeight(3);
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x+20, pointB.y);
+                image(this.slingImage3,pointA.x+25,pointA.y-15,15,30);
+                
+                
+                
+                
+            }
         }
     }
     
